fad26558a9e3ebbb1e5b96ab344c928b439a1e92
renaerdelyi <rena.szabo@yahoo.com>
2024-09-12 01:32:08 +0300

    add2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let colorChange;
+
+  //functia start btn
+
+startBtn.addEventListener('click',() => {
+    pushStart();
+    startBtn.disabled = false;
+});
+
+function pushStart(){
+    colorChange = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor();
+    },1000);
+  }
+  // functia stop btn
+
+stopBtn.addEventListener('click',()  => {
+    pushStop(); 
+ });
+
+function pushStop(){
+    clearInterval(colorChange)
+}
+ 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }

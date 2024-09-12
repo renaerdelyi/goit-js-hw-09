9d9481d739437528cdc31cf994c249a9bc23ea44
renaerdelyi <rena.szabo@yahoo.com>
2024-09-13 00:58:44 +0300

    add6

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,8 +2,8 @@ import Notiflix from "notiflix";
 
 function createPromise(position, delay) {
  return new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-setTimeout(() => {
+  setTimeout(() => {
+const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     resolve({position,delay})
   } else {
@@ -26,10 +26,10 @@ for(let i = 0 ; i < amount; i++){
   const currentDelay = delay + i * step ;
   createPromise(i + 1, currentDelay)
   .then(value => {
-    console.log(`✅ Fulfilled promise ${value.position} in ${value.delay}ms`);
+    Notiflix.Notify.success(`✅ Fulfilled promise ${value.position} in ${value.delay}ms`);
   })
   .catch(error => {
-     console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+     Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
   });
 }
 });
